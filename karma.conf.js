3870c6bded7c71071338753893dbbf75239d4ae5
Matt Lane <lane.matthew@gmail.com>
2016-03-03 15:28:14 -0800

    add karma sinon

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha','chai'],
+    frameworks: ['mocha','chai','sinon'],
 
 
     // list of files / patterns to load in the browser
@@ -67,6 +67,7 @@ module.exports = function(config) {
         'karma-chai',
         'karma-mocha-reporter',
         'karma-chrome-launcher',
+        'karma-sinon'
     ],
 
     // Concurrency level
