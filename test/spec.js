2ce0722387647e4a70103e00492d37880a0ef9cc
Matt Lane <lane.matthew@gmail.com>
2016-03-03 15:44:03 -0800

    draft tests

--- test/spec.js
+++ test/spec.js
@@ -49,12 +49,20 @@ describe("MemoryGame", function() {
       var button = document.querySelector('button');
       expect(button).to.not.be.null;
     });
+
+    it("should not have a win message", function() {
+      expect(document.body.innerText).to.not.contain('You win!');
+    });
   });
 
   describe("clicking on first tile", function() {
 
     beforeEach(function() {
       tiles[0].click();
+    });
+
+    afterEach(function() {
+      document.querySelector('button').click();
     });
 
     it("should set a background image on the clicked tile", function() {
@@ -70,8 +78,16 @@ describe("MemoryGame", function() {
 
   describe("clicking on a second tile", function() {
 
+    var countDown;
+
     beforeEach(function() {
       tiles[0].click();
+      countDown = sinon.useFakeTimers();
+    });
+
+    afterEach(function() {
+      countDown.restore();
+      document.querySelector('button').click();
     });
 
     it("should set a background image only on both clicked tiles", function() {
@@ -81,38 +97,64 @@ describe("MemoryGame", function() {
       expect(flippedTileCount()).to.equal(2);
     });
   
-    xit("should trigger a match if the data-answers are the same", function() {
+    it("should trigger a match if the data-answers are the same", function() {
       var tileIdx = 1;
       while (tiles[tileIdx].dataset.answer !== tiles[0].dataset.answer) {
         tileIdx++;
       }
       tiles[tileIdx].click();
+      countDown.tick(1000);
+      expect(flippedTileCount()).to.equal(2);
     });
 
-    xit("should not trigger a match if the data-answers are different", function() {
+    it("should not trigger a match if the data-answers are different", function() {
       var tileIdx = tiles[1].dataset.answer === tiles[0].dataset.answer ? 1 : 2;
       tiles[tileIdx].click();
+      countDown.tick(100);
+      expect(flippedTileCount()).to.equal(2);
+      countDown.tick(901);
+      expect(flippedTileCount()).to.equal(0);
     });
 
-    xit("should not trigger a match if the same card is clicked twice", function() {
-
+    it("should not trigger a match if the same card is clicked twice", function() {
+      tile[0].click();
+      countDown.tick(100);
+      expect(flippedTileCount()).to.equal(1);
+      countDown.tick(901);
+      expect(flippedTileCount()).to.equal(1);
     });
 
   });
 
   describe("end game", function() {
-    xit("should tell the player 'You win!'", function() {
-
+    it("should tell the player 'You win!'", function() {
+      var answers = Array.prototype.slice.call(tiles).map(function(tile) {
+        return tile.dataset.answer;
+      });
+      for (var i = 0; i < 8; i++) {
+        tiles[answers.indexOf(i.toString())].click();
+        tiles[answers.lastIndexOf(i.toString())].click();
+      }
+      expect(document.body.innerText).to.contain('You win!');
     });
   });
 
   describe("reset", function() {
-    xit("should shuffle the board", function() {
 
+    beforeEach(function() {
+      document.querySelector('button').click();
     });
 
-    xit("should reset the tiles", function() {
+    it("should shuffle the board", function() {
+      var answers = Array.prototype.slice.call(tiles).map(function(tile) {
+        return tile.dataset.answer;
+      });
+      expect(answers).to.not.deep.equal(['0','0','1','1','2','2','3','3','4','4','5','5','6','6','7','7']);
+      expect(answers).to.not.deep.equal(['0','1','2','3','4','5','6','7','0','1','2','3','4','5','6','7']);
+    });
 
+    it("should reset the tiles", function() {
+      expect(flippedTileCount()).to.equal(0);
     });
   });
 
