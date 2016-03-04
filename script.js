94bc43cdecd53da468d40a539ac9e44c8e34a677
Matt Lane <lane.matthew@gmail.com>
2016-03-03 21:11:27 -0800

    add solution

--- script.js
+++ script.js
@@ -3,22 +3,33 @@ var isFirstClicked = false;
 var isSecondClicked = false;
 var colors = [];
 var tiles = [];
-var images = ['https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12139772_1505254603125289_528760867_n.jpg']
+var matches = 0;
+var images = [
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12139772_1505254603125289_528760867_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/11809944_1676694042554573_495250395_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12558352_1196577663703722_704750205_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12393970_514965278663200_509732690_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12298875_990874390951233_212585837_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/11910328_1758440887716729_135946346_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12393817_575365202620951_965839874_n.jpg',
+  'https://scontent-sea1-1.cdninstagram.com/t51.2885-15/e35/12545369_191849081168083_75297221_n.jpg'
+];
+var winning = document.getElementById("winner");
 
 function checkClick(){
   if(!isFirstClicked && !isSecondClicked){
     firstClicked = this;
-    firstClicked.style.backgroundColor = firstClicked.getAttribute("data-answer");
+    firstClicked.style.backgroundImage = 'url('+images[firstClicked.getAttribute("data-answer")]+')';
     isFirstClicked = true;
   }
   else if(isFirstClicked && !isSecondClicked){
     secondClicked = this;
-    secondClicked.style.backgroundColor = secondClicked.getAttribute("data-answer");
-      if (firstClicked !== secondClicked) {
-        isSecondClicked = true;
-        setTimeout(function(){
+    secondClicked.style.backgroundImage = 'url('+images[secondClicked.getAttribute("data-answer")]+')';
+    if (firstClicked !== secondClicked) {
+      isSecondClicked = true;
+      setTimeout(function(){
         check(firstClicked,secondClicked);
-      },300);
+      },500);
     }
   }
 }
@@ -29,14 +40,16 @@ function check(a,b){
   if (answerA === answerB){
     a.removeEventListener("click", checkClick);
     b.removeEventListener("click", checkClick);
-    a.style.backgroundColor = "white";
-    b.style.backgroundColor = "white";
     reset();
+    matches++;
+    if (matches === 8) {
+      winning.innerText = "You win!"
+    }
   }
   else {
     reset();
-    a.style.backgroundColor = "grey";
-    b.style.backgroundColor = "grey";
+    a.style.backgroundImage = "";
+    b.style.backgroundImage = "";
   }
 
 }
@@ -49,42 +62,17 @@ function reset(){
 }
 
 function createGameBoard(){
+  var tiles = document.querySelectorAll('.container > div'); 
+  var indices = [0,1,2,3,4,5,6,7];
+  var randomAnswers = _.shuffle(indices.concat(indices));
   for (var i = 0; i < 16; i++) {
-    var tile = document.createElement("div");
-    tile.setAttribute("data-color",i);
-    if(i < 8){
-      tile.setAttribute("data-answer",colors[i]);
-    }
-    else {
-      tile.setAttribute("data-answer",colors[i-8]);
-    }
-    tile.addEventListener("click", checkClick);
-    tile.classList.add("gameTile");
-    tiles.push(tile);
+    tiles[i].setAttribute("data-answer",randomAnswers[i]);
+    tiles[i].addEventListener("click", checkClick);
   }
-  return tiles;
-}
-
-function addTilesToGameBoard(){
-  var tiles = _.shuffle(createGameBoard());
-  tiles.forEach(function(tile){
-    document.querySelector(".container").appendChild(tile);
-  });
-}
-
-function createColorsArray(){
-  for (var i = 0; i < 8; i++) {
-    colors.push(generateRandomColor());
-  }
-  return colors.concat(colors.slice());
-}
-
-function generateRandomColor(){
-  return "rgb(" + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + "," + Math.round(Math.random()*255) + ")";
 }
 
 function addResetListener(){
-  document.querySelector(".reset").addEventListener("click", resetBoard);
+  document.querySelector("#reset").addEventListener("click", resetBoard);
 }
 
 function resetBoard(){
@@ -92,8 +80,7 @@ function resetBoard(){
 }
 
 function initialize(){
-  createColorsArray();
-  addTilesToGameBoard();
+  createGameBoard();
   addResetListener();
 }
 
